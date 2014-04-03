85c8b06d33bf788678440249d67e71f803c4a7b9
Juan Manuel Serruya <shuank@gmail.com>
2014-04-03 22:47:28 +0200

    added mongoDB database and model for TVshows

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require("express"),
     app     = express(),
     http    = require("http"),
-    server  = http.createServer(app);
-
+    server  = http.createServer(app),
+    mongoose = require('mongoose');
+    
 app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -11,6 +12,14 @@ app.configure(function () {
 
 app.get('/', function(req, res) {
   res.send("Hello world!");
+});
+
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  } else {
+    console.log('Connected to Database');
+  }
 });
 
 server.listen(3000, function() {
